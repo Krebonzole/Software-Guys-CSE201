dcb866f4e6cd30a631c0f13a4bd90503cf7a4861
gilstojt <90064384+gilstojt@users.noreply.github.com>
2022-02-21 17:12:38 -0500

    Add files via upload

---
+++ UsernameInfo.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UsernameInfo {
+
+	public static void main(String[] args) {
+			userNameToFile();
+	}
+	
+	public static void userNameToFile() { 
+		try {
+			System.out.println("Enter Username");
+			Scanner sc = new Scanner(System.in);
+			String username = sc.next();
+			FileWriter fw = new FileWriter("ListOfUsernames.txt");
+			fw.write(username);
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("File not Found.");
+		}
+	}
+}

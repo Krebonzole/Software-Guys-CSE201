2e708474b2eb88fed41e1ac30e1cbe1198bbb563
Krebonzole <99692219+Krebonzole@users.noreply.github.com>
2022-05-03 19:06:57 -0400

    Add files via upload

--- DatabaseConnector.java
+++ DatabaseConnector.java
@@ -3,8 +3,12 @@ import java.math.*;
 import java.util.Scanner;
 
 public class DatabaseConnector {
-	static String username = "root";
-	static String password = "Otis2001!!";
+	static String username = "root";//username for the database
+	static String password = "Otis2001!!";//password for the database
+	
+	
+	//This method is first. It shows how the user will log in or create their account.
+	//If account is created, the program will ask for input and insert it into a new account row with a new accountID
 	 static void connectToAndQueryDatabase(String username, String password) throws SQLException {
 		Scanner in = new Scanner(System.in);
 	    Connection con;
@@ -17,11 +21,11 @@ public class DatabaseConnector {
 		System.out.print("Login in or Create Account? (Enter C for Creating Account, Press Any Key to Login)");
 		String answer = in.next();
 		if(answer.equals("C")) {
-			System.out.println("Enter Username:");
+			System.out.println("Enter Username:"); //new username
 		    String newUser = in.next();
-		    System.out.println("Enter Password:");
+		    System.out.println("Enter Password:"); //new password
 		    String newPass = in.next();
-		    String sql = "INSERT INTO Accounts (userName, passWord) VALUES ('%s', '%s')";
+		    String sql = "INSERT INTO Accounts (userName, passWord) VALUES ('%s', '%s')"; //inserts both into accounts
 		    sql = String.format(sql, newUser, newPass);
 		    prestat.executeUpdate(sql);
 		    System.out.print("Account Created");
@@ -32,6 +36,8 @@ public class DatabaseConnector {
 	    System.out.println("Enter Password:");
 	    String Pass = in.next();
 	    String sql = "SELECT userName, password, accountID FROM Accounts WHERE Accounts.userName = '%s' AND Accounts.password = '%s'";
+	    //logs in user and sets their accountID so that all future methods will 
+	    //only collect and store data according to aspects of the user
 	    sql = String.format(sql, User, Pass);
 	    ResultSet rs = prestat.executeQuery(sql);
 	    if(!(rs.next())) {
@@ -42,24 +48,59 @@ public class DatabaseConnector {
 	    int accId = rs.getInt("accountID");
 	    con.close();
 	    int choice = 0;
-	    while (choice != 6) {
+	    while (choice != 8) {
 	    	choice = menu();
 	    	if (choice == 1)
-	    		checkBalance(accId);
+	    		checkBalance(accId);//prints out the balances that the user has created
 	    	else if (choice == 2) {
-	    		createBalance(accId);
+	    		createBalance(accId);//creates a new balance and inserts it into the balances table, as well as links it to the user
 	    	} else if (choice == 3) {
-	    		makeTransaction(accId);
+	    		makeTransaction(accId);//creates a new transaction, taking or adding money to a balance. It stores this 
+	    		//new transaction in a table
 	    	} else if (choice == 4) {
-	    		makeBusiness(accId);
+	    		makeBusiness(accId);//creates a business owned by the account user
 	    	} else if (choice == 5) {
-	    		linkToBusiness(accId);
+	    		linkToBusiness(accId);//links a balance to a business
+	    	} else if (choice == 6) {
+	    		viewTrans(accId);//views transactions that the account user has made
+	    	} else if (choice == 7) {
+	    		checkBusiness(accId);
+	    	} else if (choice == 8){
+	    		break;
 	    	}
 	    		
 	    	
 	    }
 	    
 	    
+	}
+	 
+	private static void checkBusiness(int accId) throws SQLException {
+		Scanner in = new Scanner(System.in);
+		Connection con;
+		con = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/CSE201PROJECT?prop1",
+                username,
+                password);
+		
+		PreparedStatement upd = con.prepareStatement("blah blah blah",ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		String sql = "SELECT purpose, money, name FROM Balances bal, Business bus WHERE bal.businessID = %d AND bus.businessID =  %d AND bus.name = '%s'";
+		System.out.println("What business would you like to check?");
+		String busName = in.nextLine();
+		String sql2 = "SELECT businessID, name FROM Business WHERE name = '%s'";
+		sql2 = String.format(sql2, busName);
+		ResultSet rs = upd.executeQuery(sql2);
+		rs.first();
+		int busID = rs.getInt("businessID");
+		sql = String.format(sql, busID, busID, busName);
+		ResultSet rs2 = upd.executeQuery(sql);
+		rs2.beforeFirst();
+		while(rs2.next()) {
+	    	double mon = rs2.getDouble("money");
+	    	String purpose = rs2.getString("purpose");
+	    	System.out.println(purpose + " : " + mon);
+    	}
+		
 	}
 	
 	private static void linkToBusiness(int accId) throws SQLException {
@@ -91,6 +132,27 @@ public class DatabaseConnector {
 		upd.executeUpdate(sql1);
 		
 	}
+	
+	private static void viewTrans(int accId) throws SQLException {
+		Scanner in = new Scanner(System.in);
+		Connection con;
+		con = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/CSE201PROJECT?prop1",
+                username,
+                password);
+		
+		PreparedStatement upd = con.prepareStatement("blah blah blah",ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		String sql = "SELECT purpose, netChange FROM Transactions Where accountID = %d";
+		sql = String.format(sql, accId);
+		ResultSet rs = upd.executeQuery(sql);
+		String purpose = null;
+		double mon = 0;
+		while (rs.next()) {
+			purpose = rs.getString("purpose");
+			mon = rs.getDouble("netChange");
+			System.out.println(purpose + ": " + mon);
+		}
+	}
 
 	private static void makeBusiness(int accId) throws SQLException {
 		// TODO Auto-generated method stub
@@ -117,7 +179,9 @@ public class DatabaseConnector {
 		System.out.println("3. Make Transaction");
 		System.out.println("4. Make Business");
 		System.out.println("5. Link Balance to Business");
-		System.out.println("6. Exit");
+		System.out.println("6. View Transactions");
+		System.out.println("7. View Business Accounts");
+		System.out.println("8. Exit");
 		int ret = in.nextInt();
 		return ret;
 	}
